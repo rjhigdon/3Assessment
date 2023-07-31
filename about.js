8cd53c6010d3642b269abee536f13058cc9be8df
rjhigdon <118936868+rjhigdon@users.noreply.github.com>
2023-07-30 23:34:53 -0700

    3 Assessment Final

--- about.js
+++ about.js
@@ -1,19 +1,19 @@
-console.log("hello world");
+let image = document.querySelector("#caDuck")
+
+function caDuck(event){
+	alert('thanks for petting the caDuck')
+}
+
+image.addEventListener('mouseover', caDuck)
+
 
 let form = document.querySelector('#contact')
 let submitBtn = document.querySelector('#submit')
-	let name = document.querySelector("name")
-	let msg = document.querySelector("message")
+let name = document.querySelector("#name")
+let msg = document.querySelector("#message")
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-
-	
-	// let name.textContent = nameInput.value
-	// let msg.textContent = msgInput.value
-	
-	console.log(name.value)
-	console.log(message.value)
 	
 	message.value = ""
 	name.value = ""
@@ -22,11 +22,3 @@ function handleSubmit(evt) {
 }
 
 form.addEventListener('submit', handleSubmit);
-
-let image = document.querySelector("#caDuck")
-
-function caDuck(event){
-	alert('thanks for petting the caDuck')
-}
-
-image.addEventListener('mouseover', caDuck)
--- profile.js
+++ profile.js
@@ -1,1 +1,24 @@
-console.log('hello')
+let colorBtn = document.querySelector('#color')
+let placeBtn = document.querySelector('#place')
+let ritualBtn = document.querySelector('#ritual')
+
+function colorAlert(event, colorAl){
+    alert('My favorite color is Black!!')
+}
+
+
+let colorAl = colorBtn.addEventListener("click", colorAlert)
+
+function placeAlert(event, placeAl){
+    alert("My favorite place is Couer d'Alene!!")
+}
+
+
+let placeAl = placeBtn.addEventListener("click", placeAlert)
+
+function ritualAlert(event, ritualAl){
+    alert('My favorite ritual is drinking coffee!!')
+}
+
+
+let ritualAl = ritualBtn.addEventListener("click", ritualAlert)
